721d2f1ce8ab280807362b6b118cb0f44c4ca47c
Kaan İpşir <kaanipsir@Ki-MacBook-Pro.local>
2026-05-23 19:53:34 +0300

    fix app complete

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+from flask import Flask, request
+from twilio.rest import Client
+import os
+
+app = Flask(__name__)
+
+account_sid = os.environ.get("TWILIO_SID")
+auth_token = os.environ.get("TWILIO_TOKEN")
+
+client = Client(account_sid, auth_token)
+
+@app.route('/')
+def home():
+    return 'SMS API AKTIF'
+
 @app.route('/send', methods=['POST'])
 def send_sms():
-    try:
-        number = request.form['number']
-        text = request.form['text']
+    number = request.form['number']
+    text = request.form['text']
 
-        message = client.messages.create(
-            body=text,
-            from_='+15086875183',
-            to=number
-        )
+    message = client.messages.create(
+        body=text,
+        from_='+15086875183',
+        to=number
+    )
 
-        return {
-            "status": "success",
-            "sid": message.sid
-        }
+    return {
+        "status": "success",
+        "sid": message.sid
+    }
 
-    except Exception as e:
-        return {
-            "error": str(e)
-        }, 500
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
