1d2d10871063709b78ed2fee1ff7dd5f45a6e8cb
Kaan İpşir <kaanipsir@Ki-MacBook-Pro.local>
2026-05-23 19:59:47 +0300

    final working version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from twilio.rest import Client
 import os
 
@@ -9,25 +9,26 @@ auth_token = os.environ.get("TWILIO_TOKEN")
 
 client = Client(account_sid, auth_token)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'SMS API AKTIF'
+    return "SMS API AKTIF"
 
-@app.route('/send', methods=['POST'])
+@app.route("/send", methods=["POST"])
 def send_sms():
-    number = request.form['number']
-    text = request.form['text']
+
+    number = request.form.get("number").strip()
+    text = request.form.get("text").strip()
 
     message = client.messages.create(
         body=text,
-        from_='+15086875183',
+        from_="+15086875183",
         to=number
     )
 
-    return {
+    return jsonify({
         "status": "success",
         "sid": message.sid
-    }
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
